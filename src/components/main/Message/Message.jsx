1be0061258119b83232ae19de86bfeac8b8c73a0
OlexandrYaremchuk <70215770+OlexandrYaremchuk@users.noreply.github.com>
2022-10-22 15:39:56 +0300

    121

--- src/components/main/Message/Message.jsx
+++ src/components/main/Message/Message.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import style from './Message.module.css'
+import MessageBox from './MessageBox/MessageBox';
 
 function Message() {
     return (
 
         <div className={style.message}>
-            Message
+           <MessageBox/>
         </div>
 
 
---
+++ src/components/main/Message/MessageBox/MessageBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import style from './MessageBox.module.css'
+
+function MessageBox() {
+    return (
+
+        <div className={style.messageBox}>
+            <div className='style.Profile'>Profile</div>
+            <div className='style.Message'>Message</div>
+        </div>
+
+
+    )
+
+
+}
+
+export default MessageBox;
